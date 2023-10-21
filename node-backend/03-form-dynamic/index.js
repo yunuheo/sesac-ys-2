44c7bf619a751e292fb8647c3d715b19f924d72c
yunuheo <hyoon516@naver.com>
2023-10-21 18:30:57 +0900

    동적 이미지파일 전송

--- node-backend/03-form-dynamic/index.js
+++ node-backend/03-form-dynamic/index.js
@@ -1,36 +1,36 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8000;
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
-  res.render("index");
+app.get('/', function (req, res) {
+  res.render('index');
 });
 
-app.get("/ajax", function (req, res) {
+app.get('/ajax', function (req, res) {
   console.log(req.query);
   // { key: value, key: value }
   res.send(req.query);
 });
 
-app.post("/ajax", function (req, res) {
+app.post('/ajax', function (req, res) {
   console.log(req.body);
   res.send(req.body);
 });
 
-app.get("/axios", function (req, res) {
+app.get('/axios', function (req, res) {
   console.log(req.query);
   res.send(req.query);
 });
 
-app.post("/axios", function (req, res) {
+app.post('/axios', function (req, res) {
   console.log(req.body);
   const data = {
     ...req.body,
-    msg: "반가워요",
+    msg: '반가워요',
   };
   //   {
   //     id: ,
@@ -41,12 +41,12 @@ app.post("/axios", function (req, res) {
   res.send(data);
 });
 
-app.get("/fetch", function (req, res) {
+app.get('/fetch', function (req, res) {
   console.log(req.query);
   res.send(req.query);
 });
 
-app.post("/fetch", function (req, res) {
+app.post('/fetch', function (req, res) {
   console.log(req.body);
   res.send(req.body);
 });
---
+++ node-backend/04-file-upload/index_prac2.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const multer = require('multer'); // multer 불러오기
+const path = require('path');
+// path: 파일 경로를 받았을 때, 그 조작을 도와준다.
+// ex) 확장자 추출. 파일이름을 추출
+
+const app = express();
+const PORT = 8010;
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// 클라이언트가 uploads 폴더에 저장한 이미지파일에 접근할 수 있도록, 미들웨어 작성
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+const upload = multer({
+  dest: 'uploads/',
+});
+
+// upload라는 객체 안에는 미들웨어 함수가 존재. single(), array()
+// 미들웨어 : 요청과 응답 사이에 존재.
+// multer가 임의의 문자열을 생성해서 그 문자열을 파일 이름으로 만듦.
+// 심지어 확장자도 붙여주지 않음.
+
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, done) {
+      done(null, 'uploads/');
+    },
+    filename: function (req, file, done) {
+      console.log(file); // file.originalname : 미니언.webp
+      const ext = path.extname(file.originalname); // .webp
+      const basename = path.basename(file.originalname, ext); // 미니언
+      const fileName = basename + '_' + Date.now() + ext; // 미니언_123453156.webp
+
+      done(null, fileName);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB 제한
+});
+
+app.get('/', function (req, res) {
+  res.render('index_prac2');
+});
+
+// app.post('/upload/detail', uploadDetail.single('userfile'), function (req, res) {
+//   console.log('프로필이미지:', req.file);
+//   console.log('회원정보:', req.body);
+
+//   res.render('result_prac2', {
+//     src: req.file.path,
+//     // title: req.body.title,
+//     id: req.body.id,
+//     pw: req.body.pw,
+//     name: req.body.name,
+//     age: req.body.age,
+//   });
+//   // res.send("파일 업로드");
+// });
+
+app.post('/upload/dynamic', uploadDetail.single('userfile'), function (req, res) {
+  res.send({ src: req.file.path, id: req.body.id, pw: req.body.pw, name: req.body.name, age: req.body.age });
+});
+
+app.listen(PORT, function () {
+  console.log(`Sever Open: ${PORT}`);
+});
