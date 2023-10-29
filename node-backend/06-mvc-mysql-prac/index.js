488f5b23d5b1539d6f49ba91f763ff628bfdb7f3
heoyunu <hyoon516@naver.com>
2023-10-30 02:44:30 +0900

    mvc를 사용한 방명록 만들기 복습

--- node-backend/06-mvc-mysql-prac/index.js
+++ node-backend/06-mvc-mysql-prac/index.js
@@ -1,20 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const PORT = 8020;
+const PORT = 8000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const router = require('./routes');
+const router = require("./routes");
+app.use("/", router);
 
-app.use('/', router);
-app.use('/axios', router);
-
-app.get('*', function (req, res) {
-  res.send('페이지를 찾을 수 없습니다.');
+app.get("*", function (req, res) {
+  res.render("404");
 });
 
 app.listen(PORT, function () {
-  console.log(`Server Open ${PORT}`);
+  console.log(`Sever Open: ${PORT}`);
 });
