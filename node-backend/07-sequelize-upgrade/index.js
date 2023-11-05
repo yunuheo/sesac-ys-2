39fcbe9c8eddce39077f4b41c459d129e877652d
heoyunu <hyoon516@naver.com>
2023-11-05 22:44:15 +0900

    session로그인 복습 및 수정

--- node-backend/07-sequelize-upgrade/index.js
+++ node-backend/07-sequelize-upgrade/index.js
@@ -1,32 +1,11 @@
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-const PORT = 8050;
 
-app.set('view engine', 'ejs');
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-=======
-const port = 8000;
-// const { sequelize } = require('./model');
-
-app.set('view engine', 'ejs');
-// app.use("/static", express.static(__dirname + "/static"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
->>>>>>> ffa03025d39080c633cc03e86cf5cb5b0c1f0643
 
 const router = require('./routes');
 app.use('/', router);
 
-<<<<<<< HEAD
-app.get('*', function (req, res) {
-  res.render('404');
-});
 
-app.listen(PORT, function () {
-  console.log(`Sever Open: ${PORT}`);
-=======
 app.get('*', (req, res) => {
   res.send('접근할 수 없는 주소입니다.');
 });
@@ -47,5 +26,5 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
   console.log('Server Port : http://localhost:8000 ', port);
->>>>>>> ffa03025d39080c633cc03e86cf5cb5b0c1f0643
+
 });
